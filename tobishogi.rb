096123517dfea7319e2b1b7e5cb0b8920759718a
myokoym <myokoym@mynote02.(none)>
2011-02-13 21:25:41 +0900

    1-08

--- tobishogi.rb
+++ tobishogi.rb
@@ -1,6 +1,25 @@
 class TobiShogi
+  class Array
+    def each_with_index_reverse
+      count = self.size - 1
+      self.reverse.each do |item|
+        yield item, count
+        count -= 1
+      end
+    end
+  end
+
   def move(status)
     "222001110"
+    status.split(//).each_with_index_reverse do |v, i|
+      if v == "1"
+        if status[i - 3] == 1
+          status[i], status[i - 6] = status[i - 6], status[i]
+        elsif status[i - 3] == 0
+          status[i], status[i - 3] = status[i - 3], status[i]
+        end
+      end
+    end
   end
 end
 
