75682011f8d99df8d3bcc36f474f7fe4ae697175
myokoym <myokoym@mynote02.(none)>
2011-02-13 22:09:44 +0900

    1-17

--- tobishogi.rb
+++ tobishogi.rb
@@ -26,6 +26,15 @@ class TobiShogi
       if status[i] == "1"
         if status[i - 3] == "2"
           status[i], status[i - 6] = status[i - 6], status[i]
+          return status.join
+        end
+      end
+    end
+    8.downto(3) do |i|
+      if status[i] == "1"
+        if status[i - 3] == "0"
+          status[i], status[i - 3] = status[i - 3], status[i]
+          return status.join
         end
       end
     end
