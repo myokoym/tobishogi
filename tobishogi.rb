0be9abf6965dcc7f849e3044528a3679a2efac6b
myokoym <myokoym@mynote02.(none)>
2011-02-14 00:56:54 +0900

    1-20

--- tobishogi.rb
+++ tobishogi.rb
@@ -1,26 +1,5 @@
-class Array
-  def each_with_index_reverse
-    count = self.size - 1
-    self.reverse.each do |item|
-      yield item, count
-      count -= 1
-    end
-  end
-end
-
 class TobiShogi
   def move(state)
-    #status.split(//).each_with_index_reverse do |v, i|
-    #  if v == "1"
-    #    if status[i - 3] == "1"
-    #      status[i], status[i - 6] = status[i - 6], status[i]
-    #      break
-    #    elsif status[i - 3] == "0"
-    #      status[i], status[i - 3] = status[i - 3], status[i]
-    #      break
-    #    end
-    #  end
-    #end
     status = state.split(//)
     8.downto(6) do |i|
       if status[i] == "1"
