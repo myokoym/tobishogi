f4e1a7c5bc336bb88fde543144478848b48a0411
myokoym <myokoym@mynote02.(none)>
2011-02-13 20:43:10 +0900

    1-03

--- spec/tobishogi_spec.rb
+++ spec/tobishogi_spec.rb
@@ -3,5 +3,10 @@ require 'rspec'
 require "tobishogi"
 
 describe TobiShogi do
+  it "右手側の駒を優先する"
+  it "飛び越せるときは飛ぶ"
+  it "飛び越せないときは手前の駒を進める"
+  it "手前の駒がないときは二段目の駒を進める"
+  it "何も動かせないときはそのまま"
 end
 
