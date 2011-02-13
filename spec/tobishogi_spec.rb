2f86779a29889a7cd4153bbe28a25b273ca97018
myokoym <myokoym@mynote02.(none)>
2011-02-13 20:52:40 +0900

    1-06

--- spec/tobishogi_spec.rb
+++ spec/tobishogi_spec.rb
@@ -7,7 +7,9 @@ describe TobiShogi do
   it "右手側の駒を優先する" do
     subject.move("222000111").should == "222001110"
   end
-  it "飛び越せるときは飛ぶ"
+  it "飛び越せるときは飛ぶ" do
+    subject.move("202021110").should == "212021100"
+  end
   it "飛び越せないときは手前の駒を進める"
   it "手前の駒がないときは二段目の駒を進める"
   it "何も動かせないときはそのまま"
