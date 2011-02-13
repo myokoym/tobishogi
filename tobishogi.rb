86618b4994ec8bd07c88c7a58d54a1da280da647
myokoym <myokoym@mynote02.(none)>
2011-02-13 22:05:44 +0900

    1-16

--- tobishogi.rb
+++ tobishogi.rb
@@ -9,19 +9,27 @@ class Array
 end
 
 class TobiShogi
-  def move(status)
-    status.split(//).each_with_index_reverse do |v, i|
-      if v == "1"
-        if status[i - 3] == "1"
+  def move(state)
+    #status.split(//).each_with_index_reverse do |v, i|
+    #  if v == "1"
+    #    if status[i - 3] == "1"
+    #      status[i], status[i - 6] = status[i - 6], status[i]
+    #      break
+    #    elsif status[i - 3] == "0"
+    #      status[i], status[i - 3] = status[i - 3], status[i]
+    #      break
+    #    end
+    #  end
+    #end
+    status = state.split(//)
+    8.downto(6) do |i|
+      if status[i] == "1"
+        if status[i - 3] == "2"
           status[i], status[i - 6] = status[i - 6], status[i]
-          break
-        elsif status[i - 3] == "0"
-          status[i], status[i - 3] = status[i - 3], status[i]
-          break
         end
       end
     end
-    status
+    status.join
   end
 end
 
